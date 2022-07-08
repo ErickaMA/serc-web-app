2cb33570a0c8216be3c278870f3899014f425ca1
40228279 <40228279@ads.qub.ac.uk>
2022-07-08 15:22:36 +0100

    dog

---
+++ dog/script.js
@@ -0,0 +1,41 @@
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = 'logo.jpg'
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://dog.ceo/api/breed/shiba/images', true)
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  i = 0;
+  if (request.status >= 200 && request.status < 400) {
+    var images = data.message;
+    images.forEach(image => {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const h1 = document.createElement('h1')
+      h1.textContent = "Shiba Inu"
+
+      const img = document.createElement('img')
+      img.src = image;
+
+      container.appendChild(card)
+      card.appendChild(h1)
+      card.appendChild(img)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+}
+
+request.send()
